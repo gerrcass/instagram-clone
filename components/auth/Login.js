da4a7937c0a5a11dc90848a06411629e19c36bb9
Gerardo Castillo <gerrcass@gmail.com>
2020-12-31 12:04:33 -0400

    Adding firebase to the project and making changes to some components to interact with the firebase services...

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -3,23 +3,22 @@ import { View, Button, TextInput } from "react-native";
 
 import firebase from "firebase";
 
-export class Register extends Component {
+export class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
       password: "",
-      name: "",
     };
-    this.onSignUp = this.onSignUp.bind(this);
+    this.onSignUp = this.onSignIn.bind(this);
   }
 
-  onSignUp() {
-    const { email, password, name } = this.state;
+  onSignIn() {
+    const { email, password } = this.state;
 
     firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
+      .signInWithEmailAndPassword(email, password)
       .then((result) => {
         console.log(result);
       })
@@ -32,10 +31,6 @@ export class Register extends Component {
     return (
       <View>
         <TextInput
-          placeholder="name"
-          onChangeText={(name) => this.setState({ name })}
-        />
-        <TextInput
           placeholder="email"
           onChangeText={(email) => this.setState({ email })}
         />
@@ -44,10 +39,10 @@ export class Register extends Component {
           secureTextEntry={true}
           onChangeText={(password) => this.setState({ password })}
         />
-        <Button onPress={() => this.onSignUp()} title="Sign Up" />
+        <Button onPress={() => this.onSignIn()} title="Sign In" />
       </View>
     );
   }
 }
 
-export default Register;
+export default Login;
